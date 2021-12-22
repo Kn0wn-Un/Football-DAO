d5609ccb177706b580cd2afffc3e0e904c4e738e
Darshan H M <naikdarsh175@gmail.com>
2021-12-22 20:07:33 +0530

    Updates

---
+++ static/js/7.14ea9d76.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmydao=this.webpackJsonpmydao||[]).push([[7],{356:function(n,o){},357:function(n,o){},411:function(n,o){}}]);
+//# sourceMappingURL=7.14ea9d76.chunk.js.map
---
+++ static/js/8.d967e75e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmydao=this.webpackJsonpmydao||[]).push([[8],{356:function(n,o){},357:function(n,o){},440:function(n,o){}}]);
+//# sourceMappingURL=8.d967e75e.chunk.js.map
